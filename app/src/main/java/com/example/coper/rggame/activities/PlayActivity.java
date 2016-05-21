6c91906e95a56bb518b7ef8b4bae7699c1389c35
coperMe <dagarmo7@fiv.upv.es>
2016-05-21 21:16:47 +0200

    resquizz.xml fixed commit

--- app/src/main/java/com/example/coper/rggame/activities/PlayActivity.java
+++ app/src/main/java/com/example/coper/rggame/activities/PlayActivity.java
@@ -13,6 +13,10 @@ import android.widget.TextView;
 import com.example.coper.rggame.POJO.Riddle;
 import com.example.coper.rggame.R;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -32,22 +36,24 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        this.indexes = new int[10];
-        this.riddleList = new Vector<>();
-
         this.setupAnswerField();
         this.fillRiddleVector();
 
         if(savedInstanceState == null) {
+
             this.initIndexesArray();
 
-            this.currentRiddle = indexes[0];
+            this.currentRiddle = 0;
             this.acumScore = 0;
             this.bonusStreak = 0;
+
         }else{
-            this.bonusStreak = savedInstanceState.getInt("bonus");
+
             this.currentRiddle = savedInstanceState.getInt("current");
+            this.bonusStreak = savedInstanceState.getInt("bonus");
             this.acumScore = savedInstanceState .getInt("score");
+            this.indexes = savedInstanceState.getIntArray("indexes");
+
         }
 
         this.playGame();
@@ -66,12 +72,13 @@ public class PlayActivity extends AppCompatActivity {
     }
 
     private void initIndexesArray() {
+        this.indexes = new int[10];
         int position = 0;
 
         while (position < this.indexes.length) {
-            Double randomTaken = Math.floor(Math.random()) % (this.riddleList.size() + 1);
-            if (!Arrays.asList(this.indexes).contains(randomTaken.intValue())) {
-                this.indexes[position] = randomTaken.intValue() + 1;
+            Double randomTaken = Math.floor(Math.random()) % this.riddleList.size();
+            if (Arrays.asList(this.indexes).contains(randomTaken.intValue()) == false) {
+                this.indexes[position] = randomTaken.intValue();
                 position++;
             }
         }
@@ -91,7 +98,12 @@ public class PlayActivity extends AppCompatActivity {
 
         if(answer != null) {
 
+            /*
+             * This listener is set in order to nullify the error message when the text inside the
+             * the  editText changes.
+             */
             answer.addTextChangedListener(new TextWatcher() {
+
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -108,13 +120,38 @@ public class PlayActivity extends AppCompatActivity {
                 }
             });
 
+            /*
+             * This listener is set in order to detect when the user pushes the enter key in the
+             * keyboard and it acts like the solveButton (checking answer and refreshing the screen.
+             */
             answer.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                 @Override
                 public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                     if (actionId == EditorInfo.IME_NULL) {
+                        /*
+                        if(checkAnswer()){
+                            acumScore += 25 + 50*bonusStreak;
+                            bonusStreak++;
+                            currentRiddle++;
+
+                            if (currentRiddle < riddleList.size())
+                                drawScreen();
+                            else
+                                endGame();
+
+                        }else {
+                            EditText etAnswer = (EditText) findViewById(R.id.etAnswer);
+
+                            bonusStreak = 0;
+                            if(etAnswer != null)
+                                etAnswer.setError("So... You are not as brilliant as you thought, huh?");
+                        }
+                        */
                         return true;
-                    } else
-                        return false;
+                    }
+
+                    return false;
+
                 }
             });
         }
@@ -132,109 +169,34 @@ public class PlayActivity extends AppCompatActivity {
         }
     }
 
-    public Vector<Riddle> fillRiddleVector() {
+    public void fillRiddleVector() {
         /**
          / The first implementation of this method stored hardcoded all the riddles in a Vector of
          / riddles. Final implementation will recover the indexes stored at the String array from
          / a SQLite database, storage of the riddles.
-         / A game consists in 10 riddles with a bonus for each riddle in a row
+         / A game consists in 10 riddles with a bonus score for each riddle answered in a row
          */
-        Riddle rid;
+        XmlPullParser riddleLoader = getResources().getXml(R.xml.riddles);
+        this.riddleList = new Vector<>();
 
-        rid = new Riddle(1,
-                "We're five little items of an everyday sort. " +
-                        "You'll find us all in a tennis court",
-                "Vowels");
-        riddleList.add(rid);
-
-        rid = new Riddle(2,
-                "What is the beginning of eternity, the end of time and space, " +
-                        "the beginning of every end and the end of every race?",
-                "Letter E");
-        riddleList.add(rid);
-
-        rid = new Riddle(3,
-                "The more you take away, the larger it grows. What is it?",
-                "A hole");
-        riddleList.add(rid);
-
-        rid = new Riddle(4,
-                "Tear one off and scratch my head, what was once red is black instead.",
-                "A match");
-        riddleList.add(rid);
-
-        rid = new Riddle(5,
-                "What is it that no man wants to have but no man want to loose?",
-                "A lawsuit");
-        riddleList.add(rid);
-
-        rid = new Riddle(6,
-                "What is it that travels on all fours in the morning, " +
-                        "on two legs at noon, and three at twilight?",
-                "A man");
-        riddleList.add(rid);
-
-        rid = new Riddle(7,
-                "If you do not know where I am, then I am something, " +
-                        "but when you know where I am, I am nothing. What am I?",
-                "A riddle");
-        riddleList.add(rid);
-
-        rid = new Riddle(8,
-                "Everything in me is ancient. What am I?",
-                "A museum");
-        riddleList.add(rid);
-
-        rid = new Riddle(9,
-                "What starts at the top and then goes down, " +
-                        "has the ability to purify and has all the colours of the rainbow?",
-                "Light");
-        riddleList.add(rid);
-
-        rid = new Riddle(10,
-                "What kind of art does Aquaman prefer?",
-                "Watercolor");
-        riddleList.add(rid);
-
-        rid = new Riddle(11,
-                "It is nowhere, but it's everywhere except if something is. What is it?",
-                "Nothing");
-        riddleList.add(rid);
-
-        rid = new Riddle(12,
-                "What is strong enough to smash ships but still fears the sun?",
-                "Ice");
-        riddleList.add(rid);
-
-        rid = new Riddle(13,
-                "The poor have, the rich need it and if you eat it you'll die",
-                "Nothing");
-        riddleList.add(rid);
-
-        rid = new Riddle(14,
-                "If you know me, you'll want to share me but if you share me, " +
-                        "I'll be gone. What am I?",
-                "A secret");
-        riddleList.add(rid);
-
-        rid = new Riddle(15,
-                "I'll be right under your feet in the midday sun; you cannot leave me," +
-                        "no matter how fast you run",
-                "Shadow");
-        riddleList.add(rid);
-
-        rid = new Riddle(16,
-                "The more there is, the less you see. What could I be?",
-                "Darkness");
-        riddleList.add(rid);
-
-        rid = new Riddle(17,
-                "Hit me hard and I will crack but you'll never stop me from staring back." +
-                        "What am I?",
-                "Mirror");
-        riddleList.add(rid);
-
-        return riddleList;
+        try {
+            int riddleId=0;
+            while (riddleLoader.getEventType() != XmlPullParser.END_DOCUMENT){
+                if(riddleLoader.getEventType() == XmlPullParser.START_TAG &&
+                        riddleLoader.getName().equals("riddle")){
+                    this.riddleList.add(
+                            new Riddle(riddleId,
+                                       riddleLoader.getAttributeValue(0),
+                                       riddleLoader.getAttributeValue(1)));
+                    riddleId++;
+                }
+                riddleLoader.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickSolveButton(View v) {
@@ -272,7 +234,7 @@ public class PlayActivity extends AppCompatActivity {
         EditText answerField = (EditText) findViewById(R.id.etAnswer);
 
         String proposedAnswer = "";
-        String realAnswer = this.riddleList.get(this.currentRiddle).getAnswer();
+        String realAnswer = this.riddleList.get(this.indexes[this.currentRiddle]).getAnswer();
 
         if(answerField!=null)
             proposedAnswer = answerField.getText().toString();
